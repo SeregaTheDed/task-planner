0cd74156e0cb9471602521d78db4dd314c50aa0c
SeregaTheDed <shtanko33333@mail.ru>
2023-06-06 18:44:54 +0300

    add helloworld page and base home controller

--- src/main/java/com/shtanko/taskplanner/TaskPlannerApplication.java
+++ src/main/java/com/shtanko/taskplanner/TaskPlannerApplication.java
@@ -2,12 +2,14 @@ package com.shtanko.taskplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.shtanko.taskplanner.controllers"})
 public class TaskPlannerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskPlannerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskPlannerApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/shtanko/taskplanner/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.shtanko.taskplanner.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index.html"; // Возвращает имя представления (шаблона)
+    }
+}
