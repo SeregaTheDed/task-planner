6437ab7b99cc8f6cb2589976a1fb260858da8f76
SeregaTheDed <shtanko33333@mail.ru>
2023-06-08 01:38:44 +0300

    change task-planner to todo-list

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,20 +1,88 @@
 'use strict'
 
-function fillTable(){
-    const table = document.getElementById("calendar");
-    const rowCount = 5;
-    const columnCount = 7;
-    for (let row = 0; row < rowCount; row++) {
-        let rowNode = document.createElement('tr');
-        for (let column = 0; column < columnCount; column++) {
-            let columnNode = document.createElement('td');
-            rowNode.append(columnNode);
-        }
-        table.append(rowNode);
+
+function textAreaAdjust(element) {
+    element.style.height = "1px";
+    element.style.height = (25+element.scrollHeight)+"px";
+}
+
+function textAreaAdjustAll() {
+    var textareas = document.querySelectorAll(".task__description");
+    textareas.forEach(elem => textAreaAdjust(elem));
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    textAreaAdjustAll();
+});
+
+class Task{
+    #number;
+    #title;
+    #description;
+    #taskNode;
+    #numberNode;
+    #titleNode;
+    #descriptionNode;
+    get node() {
+        return this.#taskNode;
+    }
+    // <div class="task">
+    //   <div class="task__number">1</div>
+    //   <div class="task__content">
+    //     <div class="task__title">Записаться к врачу</div>
+    //     <textarea  class="task__description" onkeyup="textAreaAdjust(this)">Прийти в кабинет 8 по адресу ул. Пушкина и записаться к врачу</textarea>
+    //   </div>
+    // </div>
+    constructor(number, title, description){
+        this.#number = number;
+        this.#title = title;
+        this.#description = description;
+        this.initNode();
+    }
+
+    initNode() {
+        let taskNode = document.createElement('div');
+        taskNode.classList.add('task');
+
+        let numberNode = document.createElement('div');
+        numberNode.classList.add('task__number');
+        numberNode.textContent = this.#number;
+        taskNode.append(numberNode);
+
+        let taskContentNode = document.createElement('div');
+        taskContentNode.classList.add('task__content');
+        taskNode.append(taskContentNode);
+
+        let titleNode = document.createElement('div');
+        titleNode.classList.add('task__title');
+        titleNode.textContent = this.#title;
+        taskContentNode.append(titleNode);
+
+        let descriptionNode = document.createElement('textarea');
+        descriptionNode.classList.add('task__description');
+        descriptionNode.textContent = this.#description;
+        descriptionNode.setAttribute('onkeyup', 'textAreaAdjust(this)');
+        taskContentNode.append(descriptionNode);
+
+        this.#taskNode = taskNode;
+        this.#numberNode = numberNode;
+        this.#titleNode = titleNode;
+        this.#descriptionNode = descriptionNode;
+
     }
 }
 
+let tasks = [
+    new Task(1, 'Записаться к врачу', 'Прийти в кабинет 8 по адресу ул. Пушкина и записаться к врачу'),
+    new Task(2, 'Пройти собеседование', 'Зайти в специальное приложение для собеседования и пройти его'),
+    new Task(3, 'Подготовиться к экзамену', 'Очень интересная мысль меня посетила - подготовиться к экзамену. Но еще много времени для подготовки - до осени'),
+];
+
+const taskContainerNode = document.getElementById('todolist');
+
+tasks.forEach(elem => taskContainerNode.append(elem.node));
+
 document.addEventListener('DOMContentLoaded', ()=>
 {
-    fillTable();
+    //fillTable();
 });
